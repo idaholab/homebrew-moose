14d39d2bfe0b9cf0e939ed119fc3636f6cd83323
Jason Miller <jason.miller@inl.gov>
2019-09-05 16:53:43 -0600

    Fix bottle location
    Refs #000

--- Formula/moose-mesa.rb
+++ Formula/moose-mesa.rb
@@ -4,9 +4,10 @@ class MooseMesa < Formula
   homepage "https://www.mesa3d.org/"
   url "https://mesa.freedesktop.org/archive/mesa-18.2.8.tar.xz"
   sha256 "1d2ed9fd435d86d95b7215b287258d3e6b1180293a36f688e5a2efc18298d863"
-  version "18.2.8-1"
+  version "18.2.8-2"
 
   bottle do
+    root_url "https://mooseframework.org/source_packages"
     sha256 "4d0729f198648e9c4cda736eb44619ffd7deb097b5cb2520a4bf211311ac6717" => :mojave
     sha256 "f8f9a30d41adec1e1458b7db46d28f7d93a590dc8932855c9ecc65e5811f36f8" => :high_sierra
     sha256 "ad065f014ee96c4399278374db04bb1f4f4c6451cf083c8534a266ba030111ef" => :sierra
